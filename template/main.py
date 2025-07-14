50d28bf870d0cf97642f2b323c634088670bd933
el hadji <elhadji.sarr@outlook.com>
2025-07-14 17:01:02 +0200

    feat: updated jinja core template manager

--- template/main.py
+++ template/main.py
@@ -1,9 +1,8 @@
-import jinja2
-
+from jinja2 import FileSystemLoader, Environment
 class MeliaTemplate:
     
     def __init__(self):
-        self.environnement = jinja2.Environment()
+        self.environnement = Environment(loader=FileSystemLoader("templates/"))
          
     def test_jinja(self):
         test_template = self.environnement.from_string("Hello, {{name}}")
