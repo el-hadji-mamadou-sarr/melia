b5e116bf5585a499bc1172803b93a37f9b1d3877
el hadji <elhadji.sarr@outlook.com>
2025-07-14 22:43:36 +0200

    feat: updated pyproject.toml

--- cli/main.py
+++ cli/main.py
@@ -9,8 +9,8 @@ def cli():
 @click.option('--name', prompt='Name of the project', help='Name of the fastapi apploication')
 def init(name):
     template = MeliaTemplate()
-    template.make_starter_from_tree()
-    click.echo(f"Project {name} Innitialized")
+    template.make_starter_from_tree(name)
+    click.echo(f"Project {name} Initialized")
         
 
 if __name__=='__main__':
