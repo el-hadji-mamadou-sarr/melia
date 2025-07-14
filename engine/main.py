9bf397aaf8c234d3c26c25169bd21d3121d3e05f
el hadji <elhadji.sarr@outlook.com>
2025-07-14 20:26:53 +0200

    feat: added fastapi starter generator

--- engine/main.py
+++ engine/main.py
@@ -3,8 +3,7 @@ from jinja2 import FileSystemLoader, Environment
 import os
 import json
 
-STARTER_OUTPUT = "output/fastapi_starter"
-STARTER_INPUT = "fastapi_starter"
+OUTPUT = "output"
 STARTER_TREE = "templates/fastapi_starter/tree.json"
 
 class TreeFile:
@@ -12,30 +11,53 @@ class TreeFile:
         self.name = name
         
 class Tree:
-    def __init__(self, folder_name:str, sub:list[Tree], files:list[TreeFile]):
+    def __init__(self, folder_name:str, sub:list['Tree'], files:list[TreeFile]):
         self.folder_name = folder_name
         self.sub = sub
         self.files = files
+    
+    @classmethod
+    def from_dict(cls, data:dict) ->'Tree':
+        return cls(
+            folder_name=data['folder_name'],
+            sub=[cls.from_dict(item) for item in data['sub']],
+            files=[TreeFile(**item) for item in data["files"]]
+        )
    
 class MeliaTemplate:
     
     def __init__(self):
         self.environnement = Environment(loader=FileSystemLoader("templates/"))
     
-    def make_fastapi_starter(self):
-        os.makedirs(f"{STARTER_OUTPUT}/app/core", exist_ok=True)
-        os.makedirs(f"{STARTER_OUTPUT}/app/api", exist_ok=True)
+    
+    def make_files(self, tree:Tree, base=None):
+        if base:
+            base = f"{base}/{tree.folder_name}"
+        else:
+            base = tree.folder_name
         
-        template = self.environnement.get_template(f'{STARTER_INPUT}/app/main.py')
-        rendered = template.render(test="Test")
+        os.makedirs(f"{OUTPUT}/{base}", exist_ok=True)
         
-        with open("{STARTER_OUTPUT}/app/main.py", "w") as f:
-            f.write(rendered)
+         # create the files
+        for file in tree.files:
+            path = f"{base}/{file.name}"
+            template = self.environnement.get_template(path)
+            rendered = template.render()
+            
+            with open(f"{OUTPUT}/{path}", "w") as f:
+                f.write(rendered)
+        
+        # create sub
+        for sub in tree.sub:
+            self.make_files(sub, base)
     
     def make_starter_from_tree(self):
         with open(STARTER_TREE, "r") as f:
             result = f.read()
             data = json.loads(result)
-            tree = Tree(**data)
+            tree = Tree.from_dict(data)
 
-        print(tree.folder_name)
+        self.make_files(tree)
+        
+            
+        
